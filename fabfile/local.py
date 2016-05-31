3c28ca54f06587003d0cabbcd59677e568b1fbe9
Nick Kuhn <nicklz22@yahoo.com>
2016-05-31 13:57:28 -0700

    Revert

--- fabfile/local.py
+++ fabfile/local.py
@@ -134,10 +134,11 @@ def update():
 def rebuild():
     with settings(warn_only=True):
         # Install local database from dev server
-        local("cd ~/www/platform/_www && drush sql-sync @bic.phase-3 @bic._local --create-db -y --source-dump=/tmp/tmp.sql.gz --target-dump=/tmp/tmp.sql.gz")
+        
         local('sudo chmod 777 -R /home/vagrant/www/platform/.platform/local/builds/default/public')
         local('sudo rm -rf /home/vagrant/www/platform/.platform/local/builds/default/public')
         local('cd ~/www/platform/ && platform build')
+        local("cd ~/www/platform/_www && drush sql-sync @bic.phase-3 @bic._local --create-db -y --source-dump=/tmp/tmp.sql.gz --target-dump=/tmp/tmp.sql.gz")
         local('cd ~/www/platform/_www && drush @bic._local updb -y && drush @bic._local fra -y')
         local('cd ~/www/platform/_www && phpcbf')
          
